35daacfa4bd9ae45407ee5daed32b6abbc6436aa
win10 <1194051579@qq.com>
2023-08-14 23:25:10 +1000

    try

---
+++ back end/app.py
@@ -0,0 +1,30 @@
+import os
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        programming_topic = request.form["programming_topic"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(programming_topic),
+            temperature=0.6,
+            max_tokens=500  # 设置生成的最大标记数量
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+def generate_prompt(programming_topic):
+    return """Generate a code example related to {}.
+
+Topic: {}
+Code:""".format(
+        programming_topic,
+        programming_topic.capitalize()
+    )
