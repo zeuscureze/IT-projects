72e980654f5261fc40a033ed4df7d59c18745379
Tianhan Luo <tianhanl@student.unimelb.edu.au>
2023-09-18 00:09:02 +1000

    add: QAindex page(QuestionSorting?); need: QAdetailpage/QApostpage;

--- blueprints/forms.py
+++ blueprints/forms.py
@@ -36,3 +36,8 @@ class LoginForm(wtforms.Form):
     username = wtforms.StringField(validators=[Length(min=3, max=20, message="用户名格式错误！")])
     password = wtforms.StringField(validators=[Length(min=6, max=20, message="密码格式错误！ ")])
 
+
+class QuestionForm(wtforms.Form):
+    title = wtforms.StringField(validators=[Length(min=3, max=100, message="标题格式错误！ ")])
+    content = wtforms.StringField(validators=[Length(min=3, message="内容格式错误！ ")])
+
--- blueprints/qa.py
+++ blueprints/qa.py
@@ -1,6 +1,20 @@
 from flask import Blueprint, render_template, jsonify, redirect, url_for, session
+from .forms import QuestionForm
+from models import QuestionModel
 from exts import db
 from flask import request
 
 
 bp = Blueprint("qa", __name__, url_prefix="/qa")
+
+
+@bp.route("/blogs")
+def blogs():
+    questions = QuestionModel.query.order_by(QuestionModel.create_time.desc()).all()
+    return render_template("Q&A.html", questions=questions)
+
+
+@bp.route("/detail/<qa_id>")
+def detail(qa_id):
+    question = QuestionModel.query.get(qa_id)
+    return render_template("target-page.html", question=question)
